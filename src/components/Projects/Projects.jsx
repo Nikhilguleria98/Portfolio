10af6b53e94247119f3a5ec523a69d30cddb3169
Nikhil <nikhilbatiyal3891@gmail.com>
2025-01-13 18:22:05 +0530

    Added link

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import { div } from "framer-motion/client";
+import { div, link } from "framer-motion/client";
 import React from "react";
 import { IoEyeSharp } from "react-icons/io5";
 import { FaGithub } from "react-icons/fa6";
@@ -16,6 +16,8 @@ const Projects = () => {
       name: "Kreativan Technologies",
       img: "./foodweb.png",
       desciption: "nothing this time",
+      link:"foodwebsite-zeta.vercel.app",
+      gitLink:"https://github.com/Nikhilguleria98/Foodwebsite"
     },
     {
       id: 3,
@@ -48,7 +50,7 @@ const Projects = () => {
         </div>
 
         {/* Project Cards */}
-        <div className="w-full px-4  max-w-screen-lg mt-10 mb-20">
+        <div className="w-full px-[10vw] sm:px-4  max-w-screen-lg mt-10 mb-20 ">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {data.map((currElem) => (
               <div
@@ -67,13 +69,13 @@ const Projects = () => {
                   <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-md">
                     <div className="flex gap-4">
                       <a
-                        href="#"
+                        href={currElem.link ? `https://${currElem.link}` : "#"} target="_main"
                         className="text-white bg-zinc-800 opacity-75 p-4 rounded-full hover:bg-zinc-600 hover:-translate-y-2 duration-300 transition"
                       >
                         <IoEyeSharp className="text-2xl" />
                       </a>
                       <a
-                        href="#"
+                         href={currElem.link ? `https://${currElem.gitLink}` : "#"} target="_main"
                         className="text-white bg-zinc-800 opacity-75 p-4 rounded-full hover:bg-zinc-600 hover:-translate-y-2 duration-300 transition"
                       >
                         <FaGithub className="text-2xl" />
